5c8be9c3fb549f155c2beaa1aeb45bd6dfce7bdb
Anna-64 <shurygina6410@gmail.com>
2021-12-24 17:40:31 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15_000;
+        int oneMile = 20;
+        int miles = ticketPrice / oneMile;
+
+        System.out.println("Ваши мили: " + miles);
+    }
+}
